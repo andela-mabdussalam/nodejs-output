132ff215f491a588e3a987ff7df1d7554b3433ce
oyinda-subair <oyindamola.subair@andela.com>
2018-06-27 15:45:45 +0100

    create todo model and migration file

---
+++ src/server/migrations/20180627143343-create-todo.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => { // eslint-disable-line
+    return queryInterface.createTable('Todos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.ENUM('in progress', 'done', 'not started'),
+        defaultValue: 'not started'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => { // eslint-disable-line
+    return queryInterface.dropTable('Todos');
+  }
+};
---
+++ src/server/models/todo.js
@@ -0,0 +1,18 @@
+'use strict';
+
+export default (sequelize, DataTypes) => {
+  const Todo = sequelize.define('Todo', {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    status: {
+      type: DataTypes.ENUM('in progress', 'done', 'not started'),
+      defaultValue: 'not started'
+    }
+  }, {});
+  Todo.associate = (models) => {
+    // associations can be defined here
+  };
+  return Todo;
+};
